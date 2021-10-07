8d2b436256fc9bfefe9ec5e6363068639c1b1b3a
Sam King <mail@samking.co>
2021-10-07 21:10:39 +0100

    Gifs sort of working

--- gifs.js
+++ gifs.js
@@ -3,14 +3,54 @@ const path = require("path");
 const puppeteer = require("puppeteer");
 const GIFEncoder = require("gifencoder");
 const PNG = require("png-js");
+const inkjet = require("inkjet");
+const cliProgress = require("cli-progress");
 
 // Set low framerate for now, faster rendering speed
-const GIF_FPS = 25;
-const GIF_DUR = 1;
-const ORIG_DUR = 8;
+const GIF_FPS = 10;
+const GIF_DUR = 8;
+const TOTAL_FRAMES = GIF_FPS * GIF_DUR;
 const SLOWDOWN_FACTOR = 10;
 const FRAME_TIME = (1 / GIF_FPS) * 1000;
 const GIF_DIMENSION = 500;
+
+const minimalPuppeteerArgs = [
+  "--autoplay-policy=user-gesture-required",
+  "--disable-background-networking",
+  "--disable-background-timer-throttling",
+  "--disable-backgrounding-occluded-windows",
+  "--disable-breakpad",
+  "--disable-client-side-phishing-detection",
+  "--disable-component-update",
+  "--disable-default-apps",
+  "--disable-dev-shm-usage",
+  "--disable-domain-reliability",
+  "--disable-extensions",
+  "--disable-features=AudioServiceOutOfProcess",
+  "--disable-hang-monitor",
+  "--disable-ipc-flooding-protection",
+  "--disable-notifications",
+  "--disable-offer-store-unmasked-wallet-cards",
+  "--disable-popup-blocking",
+  "--disable-print-preview",
+  "--disable-prompt-on-repost",
+  "--disable-renderer-backgrounding",
+  "--disable-setuid-sandbox",
+  "--disable-speech-api",
+  "--disable-sync",
+  "--hide-scrollbars",
+  "--ignore-gpu-blacklist",
+  "--metrics-recording-only",
+  "--mute-audio",
+  "--no-default-browser-check",
+  "--no-first-run",
+  "--no-pings",
+  "--no-sandbox",
+  "--no-zygote",
+  "--password-store=basic",
+  "--use-gl=swiftshader",
+  "--use-mock-keychain",
+];
 
 function decode(png) {
   return new Promise((r) => {
@@ -36,7 +76,8 @@ function sleep(ms) {
 
   const data = [];
   for (const [tokenId, attributes] of Object.entries(flowers)) {
-    if (attributes.spin) {
+    if (tokenId === "108") {
+      // if (attributes.spin) {
       data.push({
         tokenId,
         image: images[tokenId],
@@ -44,12 +85,13 @@ function sleep(ms) {
     }
   }
 
-  // Start browser to convert svg to png
+  // Start headless browser to convert svg to png
   const browser = await puppeteer.launch({
     headless: true,
-    slowMo: 0,
+    args: minimalPuppeteerArgs,
   });
 
+  // Open the headless browser page
   const page = await browser.newPage();
   await page.setViewport({
     height: GIF_DIMENSION,
@@ -58,6 +100,10 @@ function sleep(ms) {
 
   // Generate gif files
   for (const token of data) {
+    // Set logging things
+    const renderProgress = new cliProgress.SingleBar();
+    const encodeProgress = new cliProgress.SingleBar();
+
     // Set up a new gif encoder
     const encoder = new GIFEncoder(GIF_DIMENSION, GIF_DIMENSION);
     encoder
@@ -68,46 +114,69 @@ function sleep(ms) {
         )
       );
 
-    console.log(`Generating gif for flower #${token.tokenId}`);
-
-    // Grab the base64 data and parse out the SVG file
-    const base64 = token.image;
-    let svg = Buffer.from(base64.substring(26), "base64").toString();
-    // Slow down the animation so puppeteer has time to get and save screenshots
-    svg = svg.split('dur="8s"').join(`dur="${ORIG_DUR * SLOWDOWN_FACTOR}s"`);
-    // Render the svg to the headless browser
-    await page.setContent(
-      `<!DOCTYPE html><style>* { margin: 0; padding: 0; }</style>${svg}`,
-      { waitUntil: "networkidle2" }
-    );
-
     // Start the encoder
     encoder.start();
     encoder.setRepeat(0);
     encoder.setDelay(FRAME_TIME);
     encoder.setQuality(10);
 
+    // Grab the base64 data and parse out the SVG file
+    const base64 = token.image;
+    let svg = Buffer.from(base64.substring(26), "base64").toString();
+
+    // Slow down animation so puppeteer has time to get and save screenshots
+    svg = svg.split('dur="8s"').join(`dur="${GIF_DUR * SLOWDOWN_FACTOR}s"`);
+
+    // Render the svg to the headless browser
+    await page.setContent(
+      `<!DOCTYPE html><style>* { margin: 0; padding: 0; }</style>${svg}`
+    );
+
+    // Delay the start of the recording to wait for loading, we can't use the
+    // `page.waitForSelector` because it doesn't work with `page.setContent`
+    await sleep(1000);
+
+    // Logging
+    console.log(`Rendering flower #${token.tokenId}`);
+    renderProgress.start(TOTAL_FRAMES, 0);
+
     // Store frames for encoding later
     // This helps to not lag puppeteer if we delay encoding
-    const pngFrames = [];
+    const frames = [];
 
     // Grab a screenshot of each frame
-    for (let frame = 0; frame < GIF_FPS * GIF_DUR; frame++) {
-      const pngBuffer = page.screenshot({
+    for (let frame = 0; frame < TOTAL_FRAMES; frame++) {
+      renderProgress.update(frame + 1);
+      const frameBuffer = page.screenshot({
         clip: { width: GIF_DIMENSION, height: GIF_DIMENSION, x: 0, y: 0 },
+        type: "jpeg",
+        quality: 100,
       });
-      pngFrames.push(pngBuffer);
+      frames.push(frameBuffer);
       await sleep(FRAME_TIME * SLOWDOWN_FACTOR);
     }
 
-    for (const pngFrame of pngFrames) {
-      const frame = await pngFrame;
-      const png = new PNG(frame);
-      await decode(png).then((pixels) => encoder.addFrame(pixels));
+    // Logging
+    renderProgress.stop();
+    console.log(`Generating gif for flower #${token.tokenId}`);
+    encodeProgress.start(frames.length, 0);
+
+    for (let i = 0; i < frames.length; i++) {
+      encodeProgress.update(i + 1);
+      const frame = await frames[i];
+      inkjet.decode(frame, (err, { data }) => {
+        if (!err) {
+          encoder.addFrame(data);
+        }
+      });
+
+      // For when using PNG instead of JPG
+      // const png = new PNG(frame);
+      // await decode(png).then((pixels) => encoder.addFrame(pixels)});
     }
 
     encoder.finish();
-    console.log(`Done with ${token.tokenId}`);
+    encodeProgress.stop();
   }
 
   // Clean up the headless browser
