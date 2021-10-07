072263ae64500b5041cf2116aa39bd790e583fb5
Sam King <mail@samking.co>
2021-10-07 18:53:18 +0100

    Change gif generation script

--- gifs.js
+++ gifs.js
@@ -1,59 +1,29 @@
 const fs = require("fs");
 const path = require("path");
+const puppeteer = require("puppeteer");
 const GIFEncoder = require("gifencoder");
-const { convert } = require("convert-svg-to-png");
-const { createCanvas, loadImage } = require("canvas");
+const PNG = require("png-js");
 
 // Set low framerate for now, faster rendering speed
-const GIF_FPS = 10;
+const GIF_FPS = 25;
 const GIF_DUR = 1;
+const ORIG_DUR = 8;
+const SLOWDOWN_FACTOR = 10;
+const FRAME_TIME = (1 / GIF_FPS) * 1000;
+const GIF_DIMENSION = 500;
 
-class SvgGif {
-  constructor({ width = 500, height = 500, fileName, repeat = 0 }) {
-    this.width = width;
-    this.height = height;
+function decode(png) {
+  return new Promise((r) => {
+    png.decode((pixels) => r(pixels));
+  });
+}
 
-    this.encoder = new GIFEncoder(width, height);
-
-    this.encoder
-      .createReadStream()
-      .pipe(
-        fs.createWriteStream(path.join(process.cwd(), "./gifs/", fileName))
-      );
-
-    // GIF encoder setup
-    this.encoder.start();
-    this.encoder.setRepeat(repeat); // 0 for repeat, -1 for no-repeat
-    this.encoder.setDelay((1 / GIF_FPS) * 1000); // frame delay in ms
-    this.encoder.setQuality(10);
-
-    // Canvas for rendering frames to
-    this.canvas = createCanvas(width, height);
-    this.ctx = this.canvas.getContext("2d");
-  }
-
-  async addFrame(svg) {
-    // Convert the svg to png using a headless browser
-    const pngData = await convert(svg, {
-      width: this.width,
-      height: this.height,
-    });
-    // Convert the png to base64 so we can load it into canvas
-    const base64 = `data:image/svg+xml;base64,${pngData.toString("base64")}`;
-    // Load and draw the image
-    const image = await loadImage(base64);
-    this.ctx.drawImage(image, 0, 0, this.width, this.height);
-    // Add to the gif
-    this.encoder.addFrame(this.ctx);
-  }
-
-  finish() {
-    this.encoder.finish();
-  }
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 (async () => {
-  // Load and normalise the data
+  // Load the data
   const flowersData = fs.readFileSync(
     path.join(process.cwd(), "./output/flowers.json")
   );
@@ -74,46 +44,74 @@ class SvgGif {
     }
   }
 
-  console.log(data);
+  // Start browser to convert svg to png
+  const browser = await puppeteer.launch({
+    headless: true,
+    slowMo: 0,
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({
+    height: GIF_DIMENSION,
+    width: GIF_DIMENSION,
+  });
 
   // Generate gif files
   for (const token of data) {
+    // Set up a new gif encoder
+    const encoder = new GIFEncoder(GIF_DIMENSION, GIF_DIMENSION);
+    encoder
+      .createWriteStream()
+      .pipe(
+        fs.createWriteStream(
+          path.join(process.cwd(), `gifs/${token.tokenId}.gif`)
+        )
+      );
+
+    console.log(`Generating gif for flower #${token.tokenId}`);
+
     // Grab the base64 data and parse out the SVG file
     const base64 = token.image;
     let svg = Buffer.from(base64.substring(26), "base64").toString();
-
-    // Start a new svg to gif encoder
-    const gif = new SvgGif({
-      width: 500,
-      height: 500,
-      fileName: `${token.tokenId}.gif`,
-    });
-
-    console.log(`Generating gif for ${token.tokenId}`);
-
-    // Set up the number of frames
-    const framesArr = Array.from(
-      { length: GIF_FPS * GIF_DUR },
-      (_, i) => i + 1
+    // Slow down the animation so puppeteer has time to get and save screenshots
+    svg = svg.split('dur="8s"').join(`dur="${ORIG_DUR * SLOWDOWN_FACTOR}s"`);
+    // Render the svg to the headless browser
+    await page.setContent(
+      `<!DOCTYPE html><style>* { margin: 0; padding: 0; }</style>${svg}`,
+      { waitUntil: "domcontentloaded" }
     );
 
-    // For each frame
-    for (const frameNumber of framesArr) {
-      const frameTime = 1 / GIF_FPS;
-      // Calculate the offset to start the animation
-      const offsetMs = Math.round(
-        (frameNumber * frameTime + Number.EPSILON) * 1000
-      );
-      // Change the original SVG to include the new offset
-      const frame = svg.split('begin="0s"').join(`begin="${offsetMs}ms"`);
-      // Render the frame
-      await gif.addFrame(frame, frameNumber);
+    // Start the encoder
+    encoder.start();
+    encoder.setRepeat(0);
+    encoder.setDelay(FRAME_TIME);
+    encoder.setQuality(10);
+
+    // Store frames for encoding later
+    // This helps to not lag puppeteer if we delay encoding
+    const pngFrames = [];
+
+    // Grab a screenshot of each frame
+    for (let frame = 0; frame < GIF_FPS * GIF_DUR; frame++) {
+      const pngBuffer = page.screenshot({
+        clip: { width: GIF_DIMENSION, height: GIF_DIMENSION, x: 0, y: 0 },
+      });
+      pngFrames.push(pngBuffer);
+      await sleep(FRAME_TIME * SLOWDOWN_FACTOR);
     }
 
-    // Finish gif rendering
-    gif.finish();
+    for (const pngFrame of pngFrames) {
+      const frame = await pngFrame;
+      const png = new PNG(frame);
+      await decode(png).then((pixels) => encoder.addFrame(pixels));
+    }
+
+    encoder.finish();
     console.log(`Done with ${token.tokenId}`);
   }
+
+  // Clean up the headless browser
+  await browser.close();
 
   console.log("Saved all the flower gifs into gifs folder");
 })().catch((err) => {
